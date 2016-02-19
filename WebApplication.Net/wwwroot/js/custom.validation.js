f3766a50179852132df78f624831fd15e522c462
TioDuke <tioduke@gmail.com>
2016-02-19 07:34:47 -0500

    correct file reference in custom.validation.js

--- WebApplication.Net/wwwroot/js/custom.validation.js
+++ WebApplication.Net/wwwroot/js/custom.validation.js
@@ -1,4 +1,4 @@
-/// <reference path="../lib/jquery-validation/jquery.validate.js" />
+/// <reference path="../lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="../lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
 
 jQuery.validator.addMethod("nasvalidation", function (value, element) {
