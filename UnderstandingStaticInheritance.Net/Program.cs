043886af7d0446ea9ef1c09237908b0e3b7997b4
TioDuke <tioduke@gmail.com>
2016-11-20 17:35:12 -0500

    fix namespace

--- UnderstandingStaticInheritance.Net/Program.cs
+++ UnderstandingStaticInheritance.Net/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace UnderstandindStaticInheritance
+namespace UnderstandindStaticInheritance.Net
 {
     public class Program
     {
