6dc4d7c17fe1ae966641fa0af4395caff1b79283
TioDuke <tioduke@gmail.com>
2017-03-05 20:27:49 -0500

    Program

--- WebApplication.Net/src/Program.cs
+++ WebApplication.Net/src/Program.cs
@@ -1,6 +1,5 @@
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 
 namespace WebApplication.Net
 {
@@ -8,13 +7,7 @@ namespace WebApplication.Net
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
-
             var host = new WebHostBuilder()
-                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
--- WebApplication.Net/src/Startup.cs
+++ WebApplication.Net/src/Startup.cs
@@ -12,7 +12,7 @@ namespace WebApplication.Net
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
