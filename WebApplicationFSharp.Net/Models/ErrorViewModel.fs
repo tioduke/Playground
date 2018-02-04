e52090348f4edcbbd906fa5221aae4092358a8d5
TioDuke <tioduke@gmail.com>
2018-02-03 19:24:41 -0500

    create WebApplicationFSharp.Net/WebApplicationFSharp.Net.Tests projets and workspace

---
+++ WebApplicationFSharp.Net/Models/ErrorViewModel.fs
@@ -0,0 +1,8 @@
+namespace WebApplicationFSharp.Net
+
+open System
+
+type ErrorViewModel private () =
+    member val RequestId : string = null with get, set
+
+    member val ShowRequestId : bool = true with get, set
