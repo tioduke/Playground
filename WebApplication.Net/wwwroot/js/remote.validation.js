0ee45fecd6bef5c6714e3266270af9db8d02fa1f
TioDuke <tioduke@gmail.com>
2018-02-05 22:47:37 -0500

    sync with f# project

---
+++ WebApplication.Net/wwwroot/js/remote.validation.js
@@ -0,0 +1,24 @@
+// I hate naming things
+function initializeRemotelyValidatingElementsWithAdditionalFields($form) {
+    var remotelyValidatingElements = $form.find("[data-val-remote]");
+
+    $.each(remotelyValidatingElements, function (i, element) {
+        var $element = $(element);
+
+        var additionalFields = $element.attr("data-val-remote-additionalfields");
+
+        if (additionalFields.length == 0) return;
+
+        var rawFieldNames = additionalFields.split(",");
+
+        var fieldNames = $.map(rawFieldNames, function (fieldName) { return fieldName.replace("*.", ""); });
+
+        $.each(fieldNames, function (i, fieldName) {
+            $form.find("#" + fieldName).change(function () {
+                // force re-validation to occur
+                $element.removeData("previousValue");
+                $element.valid();
+            });
+        });
+    });
+}
