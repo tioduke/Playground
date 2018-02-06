1970de4440df80f9ee72fc71f808ffe625d2b12f
TioDuke <tioduke@gmail.com>
2018-02-05 22:34:08 -0500

    correct remote validation + use more F# syntax

--- WebApplicationFSharp.Net/Areas/Customers/Controllers/CustomerController.fs
+++ WebApplicationFSharp.Net/Areas/Customers/Controllers/CustomerController.fs
@@ -18,23 +18,23 @@ type CustomerController () =
                 CustomerName = "Asterix",
                 NAS = "046454286",
                 Amount = Nullable 90.34M,
-                BirthDate = Nullable (DateTime(1008, 11, 01))
-            );
+                BirthDate = (Nullable <| DateTime(1008, 11, 01))
+            )
             Customer (
                 Id = Nullable 11,
                 CustomerCode = "1002",
                 CustomerName = "Obelix",
                 NAS = "046454286",
                 Amount = Nullable 91.00M,
-                BirthDate = Nullable (DateTime(972, 10, 05))
-            );
+                BirthDate = (Nullable <| DateTime(972, 10, 05))
+            )
             Customer (
                 Id = Nullable 0,
                 CustomerCode = "0000",
                 CustomerName = "",
                 NAS = "000000000",
                 Amount = Nullable 0.00M,
-                BirthDate = Nullable (DateTime(0001, 01, 01))
+                BirthDate = (Nullable <| DateTime(0001, 01, 01))
             )
         ]
 
@@ -45,25 +45,24 @@ type CustomerController () =
     // POST: /Customers/Customer/DisplayCustomer
     [<HttpPost>]
     member this.DisplayCustomer (obj: Customer) =
-        if this.ModelState.IsValid then
-            this.View("DisplayCustomer", obj) :> ActionResult
-        else
-            this.ModelState.AddModelError("Customer Form", "There are errors in the form, please correct.")
-            this.View("CustomerForm", obj) :> ActionResult
+        match this.ModelState.IsValid with
+        | true -> this.View("DisplayCustomer", obj) :> ActionResult
+        | false -> this.ModelState.AddModelError("Customer Form", "There are errors in the form, please correct.") |> ignore
+                   this.View("CustomerForm", obj) :> ActionResult
 
     // GET: /Customers/Customer/DisplayCustomer
-    member this.DisplayCustomer (id: int) =
-        let findDefault customerList: Customer =
-            List.find (fun x -> x.Id ?= 0) customerList
-        let findById customerId customerList: Customer option =
-            List.tryFind (fun x -> x.Id ?= customerId) customerList
+    member this.DisplayCustomer (id) =
+        let findDefault (customerList: Customer list) =
+            customerList |> List.find (fun x -> x.Id ?= 0)
+        let findById customerId (customerList: Customer list) =
+            customerList |> List.tryFind (fun x -> x.Id ?= customerId)
 
         defaultArg (findById id _customers) (findDefault _customers) |> this.DisplayCustomer
 
     // GET: /Customers/Customer/IsAmountValide
-    member this.IsAmountValide (obj: Customer) =
-        match obj.NAS with
+    member this.IsAmountValide (nas: string, amount: Nullable<decimal>) =
+        match nas with
         | null -> this.Json(true)
-        | _ -> match obj.Amount with
-               | x when x ?= 0M -> this.Json(false)
-               | _ -> this.Json(true)
+        | _ -> match amount with
+               | x when x ?> 0.00M -> this.Json(true)
+               | _ -> this.Json(false)
