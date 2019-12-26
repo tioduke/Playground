ed8ae1277db3d593462748b0d1f481c39cdd7911
TioDuke <tioduke@gmail.com>
2019-12-26 09:30:39 -0500

    correct usings

--- FrameworkPrototype.Net/WebAPIApplication.Net.Tests/Controllers/CustomerControllerTest.cs
+++ FrameworkPrototype.Net/WebAPIApplication.Net.Tests/Controllers/CustomerControllerTest.cs
@@ -1,13 +1,10 @@
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Xunit;
 using Xunit.Categories;
 
 using WebAPIApplication.Net.Entities;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using System.Collections.Generic;
-using System.Net;
 
 namespace WebAPIApplication.Net.Tests.Controllers
 {
