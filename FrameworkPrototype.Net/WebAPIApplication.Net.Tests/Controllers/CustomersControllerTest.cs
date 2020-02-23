8f5bb1035fc3755b5e0f5d6df256d6d371328434
TioDuke <tioduke@gmail.com>
2020-02-23 02:23:19 -0500

    tests ProblemDetails

---
+++ FrameworkPrototype.Net/WebAPIApplication.Net.Tests/Controllers/CustomersControllerTest.cs
@@ -0,0 +1,76 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Xunit;
+using Xunit.Categories;
+
+using WebAPIApplication.Net.Entities;
+
+namespace WebAPIApplication.Net.Tests.Controllers
+{
+    public class CustomersControllerTest
+    {
+        private readonly HttpClient _client;
+
+        public CustomersControllerTest()
+        {
+            _client = new HttpClient();
+        }
+
+        [Fact, SystemTest]
+        public async Task Get_TooManyResults_RetunsRequestedRangeNotSatisfiable()
+        {
+            //Arrange
+
+            //Act
+            var response = await _client.GetAsync("http://localhost:5000/api/Customers");
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.True(response.Content.IsContentTypeProblemJson());
+            var content = await response.Content.ReadAsProblemDetails();
+            Assert.IsType<ProblemDetails>(content);
+            Assert.Equal(HttpStatusCode.RequestedRangeNotSatisfiable, response.StatusCode);
+       }
+
+        [Fact, SystemTest]
+        public async Task Post_InvalidCustomer_RetunsBadRequest()
+        {
+            //Arrange
+            var customer = new Customer();
+
+            //Act
+            var response = await _client.PostAsJsonAsync("http://localhost:5000/api/Customers", customer);
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.True(response.Content.IsContentTypeProblemJson());
+            var content = await response.Content.ReadAsProblemDetails();
+            Assert.IsType<ValidationProblemDetails>(content);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+       }
+    }
+
+    internal static class HttpContextExtensions
+    {
+        internal static bool IsContentTypeProblemJson(this HttpContent httpContent)
+        {
+            return httpContent == null
+                ? false
+                : httpContent.Headers.ContentType.MediaType == "application/problem+json";
+        }
+
+        internal static async Task<ProblemDetails> ReadAsProblemDetails(this HttpContent httpContent)
+        {
+            var content = await httpContent.ReadAsStringAsync();
+            var details = JsonConvert.DeserializeObject<ValidationProblemDetails>(content);
+
+            return details.Errors.Any()
+                ? details
+                : JsonConvert.DeserializeObject<ProblemDetails>(content);
+        }
+    }
+}
