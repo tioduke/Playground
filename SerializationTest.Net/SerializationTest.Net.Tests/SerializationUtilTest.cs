3062fcd71d0491e2c3af537dc588d760debabce2
TioDuke <tioduke@gmail.com>
2020-04-14 19:30:20 -0400

    add tests decimal to string conversion

--- SerializationTest.Net/SerializationTest.Net.Tests/SerializationUtilTest.cs
+++ SerializationTest.Net/SerializationTest.Net.Tests/SerializationUtilTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text;
 using AutoFixture;
 using Xunit;
 
@@ -36,6 +38,65 @@ namespace SerializationTest.Net.Tests
 
             //Assert
             Assert.Equal(obj, copy);
+        }
+
+        [Fact]
+        public void ConvertToString_ValueLessThanZero_ConversionOK()
+        {
+            //Arrange
+            var value = -12.34m;
+
+            //Act
+            var result = ConvertToString(value);
+
+            //Assert
+            Assert.Equal("00000000000001234-", result);
+        }
+
+        [Fact]
+        public void ConvertToString_ValueLessThanZeroAndFloored_ConversionOK()
+        {
+            //Arrange
+            var value = -12.346m;
+
+            //Act
+            var result = ConvertToString(value);
+
+            //Assert
+            Assert.Equal("00000000000001234-", result);
+        }
+
+        [Fact]
+        public void ConvertToString_ValueLGreaterThanZero_ConversionOK()
+        {
+            //Arrange
+            var value = +12.34m;
+
+            //Act
+            var result = ConvertToString(value);
+
+            //Assert
+            Assert.Equal("00000000000001234+", result);
+        }
+
+        [Fact]
+        public void ConvertToString_ValueLGreaterThanZeroAndFloored_ConversionOK()
+        {
+            //Arrange
+            var value = +12.346m;
+
+            //Act
+            var result = ConvertToString(value);
+
+            //Assert
+            Assert.Equal("00000000000001234+", result);
+        }
+
+        private string ConvertToString(decimal value)
+        {
+            return new StringBuilder()
+                .Append(Convert.ToString(Math.Floor(Math.Abs(value * 100))))
+                .Append(value < 0.0m ? "-" : "+").ToString().PadLeft(18, '0');
         }
     }
 }
