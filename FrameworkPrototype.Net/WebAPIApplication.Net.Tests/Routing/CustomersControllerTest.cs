a6cd26e5babac3a31dbcf4568de283a173aa26d3
TioDuke <tioduke@gmail.com>
2020-05-19 22:39:09 -0400

    add more routing tests

--- FrameworkPrototype.Net/WebAPIApplication.Net.Tests/Routing/CustomersControllerTest.cs
+++ FrameworkPrototype.Net/WebAPIApplication.Net.Tests/Routing/CustomersControllerTest.cs
@@ -60,8 +60,44 @@ namespace WebAPIApplication.Net.Tests.Routing
             //Assert
             MyRouting
                 .Configuration()
-                .ShouldMap("/api/Customers")
+                .ShouldMap(req => req
+                    .WithPath("/api/Customers")
+                    .WithMethod(HttpMethod.Post)
+                    .WithJsonBody("{}"))
                 .To<CustomersController>(c => c.Post(new Entities.Customer()));
+        }
+
+        [Fact, IntegrationTest]
+        public void Put_UrlCustomersWithId_CallsCorrespondingControllerAction()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            MyRouting
+                .Configuration()
+                .ShouldMap(req => req
+                    .WithPath("/api/Customers/5")
+                    .WithMethod(HttpMethod.Put)
+                    .WithJsonBody("{}"))
+                .To<CustomersController>(c => c.Put(5, new Entities.Customer()));
+        }
+
+        [Fact, IntegrationTest]
+        public void Delete_UrlCustomersWithId_CallsCorrespondingControllerAction()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            MyRouting
+                .Configuration()
+                .ShouldMap(req => req
+                    .WithPath("/api/Customers/5")
+                    .WithMethod(HttpMethod.Delete))
+                .To<CustomersController>(c => c.Delete(5));
         }
     }
 }
