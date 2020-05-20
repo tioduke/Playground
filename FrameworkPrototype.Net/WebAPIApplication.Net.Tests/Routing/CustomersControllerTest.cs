94a24b177a98365f7c9d3ef5182502fd47919bb6
TioDuke <tioduke@gmail.com>
2020-05-19 22:19:37 -0400

    add more routing tests

--- FrameworkPrototype.Net/WebAPIApplication.Net.Tests/Routing/CustomersControllerTest.cs
+++ FrameworkPrototype.Net/WebAPIApplication.Net.Tests/Routing/CustomersControllerTest.cs
@@ -9,7 +9,21 @@ namespace WebAPIApplication.Net.Tests.Routing
     public class CustomersControllerTest
     {
         [Fact, IntegrationTest]
-        public void Get_ValidRouteWithId_CallsCorrespondingControllerAction()
+        public void Get_UrlCustomers_CallsCorrespondingControllerAction()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            MyRouting
+                .Configuration()
+                .ShouldMap("/api/Customers")
+                .To<CustomersController>(c => c.Get());
+        }
+
+        [Fact, IntegrationTest]
+        public void Get_UrlCustomersWithId_CallsCorrespondingControllerAction()
         {
             //Arrange
 
@@ -20,6 +34,34 @@ namespace WebAPIApplication.Net.Tests.Routing
                 .Configuration()
                 .ShouldMap("/api/Customers/5")
                 .To<CustomersController>(c => c.Get(5));
+        }
+
+        [Fact, IntegrationTest]
+        public void Get_UrlCustomersWithCode_CallsCorrespondingControllerAction()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            MyRouting
+                .Configuration()
+                .ShouldMap("/api/Customers/code/A")
+                .To<CustomersController>(c => c.Get("A"));
+        }
+
+        [Fact, IntegrationTest]
+        public void Post_UrlCustomers_CallsCorrespondingControllerAction()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            MyRouting
+                .Configuration()
+                .ShouldMap("/api/Customers")
+                .To<CustomersController>(c => c.Post(new Entities.Customer()));
         }
     }
 }
